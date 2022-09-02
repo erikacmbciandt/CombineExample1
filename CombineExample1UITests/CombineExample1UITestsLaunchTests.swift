42a3c77bb8b412708a2bb5aaf787cd3eb08b23cc
Erika Bueno <erikab@ciandt.com>
2022-09-02 10:43:06 -0300

    Initial Commit

---
+++ CombineExample1UITests/CombineExample1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CombineExample1UITestsLaunchTests.swift
+//  CombineExample1UITests
+//
+//  Created by Erika C. Matesz Bueno on 02/09/22.
+//
+
+import XCTest
+
+class CombineExample1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
